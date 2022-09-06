897b2933a94f1437ef2b1b0f5b873c9b3e91549e
y-kum <delhiiityash@gmail.com>
2022-09-06 09:22:06 +0530

    Adding java files and dependencies

---
+++ src/main/java/com/mycompany/hospital_mgmt_system/Hospital_Mgmt_System.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.hospital_mgmt_system;
+
+/**
+ *
+ * @author Vaibhav
+ */
+public class Hospital_Mgmt_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
